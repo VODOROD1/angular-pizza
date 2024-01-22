8c01d472f70d65bc0a164068ba2da185b5ecb440
mikhail.belyakov <mikhail.belyakov@corp.codeinside.ru>
2024-01-22 17:37:38 +0400

    [january_22] 'cart.reducers.ts' was refactored

--- src/app/ngrx/reducers/cart.reducers.ts
+++ src/app/ngrx/reducers/cart.reducers.ts
@@ -1,7 +1,7 @@
+import { calcTotalPrice } from "../../utils/calcTotalPrice";
 import { initialStateCommon } from "../initial-state";
 import { IState } from "../interfaces";
 import { CartActions, cartActionsType } from "./cart.actions";
-
 export const cartNode = 'cart';
 
 const initialState: IState = initialStateCommon;
@@ -9,39 +9,44 @@ const initialState: IState = initialStateCommon;
 export const cartReducer = (state = initialState, action: CartActions) => {
   switch (action.type) {
     case cartActionsType.addIncrease: {
+        let newState = {...state};
         const findItem = state.cartState.items.find((obj) => obj.id === action.payload.id);
         if (findItem) {
-            findItem.count++;
+            const indexFindedItem = state.cartState.items.indexOf(findItem);
+            newState.cartState.items[indexFindedItem].count++;
         } else {
-            state.cartState.items.push({
+            newState.cartState.items.push({
             ...action.payload,
             count: 1,
             });
         }
 
-        state.cartState.totalPrice = calcTotalPrice(state.items);
-        return {
-            ...state,
-            cartState: {...state.cartState}
-        };
+        newState.cartState.totalPrice = calcTotalPrice(newState.cartState.items);
+        return newState;
     }
     case cartActionsType.decrease: {
-        return {
-            ...state,
-            cartState: {...state.cartState}
-        };
+        let newState = {...state};
+        const findItem = state.cartState.items.find((obj) => obj.id === action.payload.id);
+
+        if (findItem) {
+            const indexFindedItem = state.cartState.items.indexOf(findItem);
+            newState.cartState.items[indexFindedItem].count--;
+        }
+
+        newState.cartState.totalPrice = calcTotalPrice(newState.cartState.items);
+        return newState;
     }
     case cartActionsType.removeItem: {
-        return {
-            ...state,
-            cartState: {...state.cartState, cart: 0}
-        };
+        let newState = {...state};
+        newState.cartState.items = state.cartState.items.filter((obj) => obj.id !== action.payload.id);
+        newState.cartState.totalPrice = calcTotalPrice(newState.cartState.items);
+        return newState;
     }
     case cartActionsType.clearItems: {
-        return {
-            ...state,
-            cartState: {...state.cartState, cart: 0}
-        };
+        let newState = {...state};
+        newState.cartState.items = [];
+        newState.cartState.totalPrice = 0;
+        return newState;
     }
     case cartActionsType.updatedAt: {
         return {
